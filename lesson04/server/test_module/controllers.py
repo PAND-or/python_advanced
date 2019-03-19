c6a250879bc1ba4541bfa8b136a9bde995c2cc6d
PAND-or <PAND-or@yandex.ru>
2019-03-19 03:17:36 +0300

    to root directry

--- lesson04/server/test_module/controllers.py
+++
@@ -1,5 +0,0 @@
-def send_presence(request):
-   return {
-            "response": 200,
-            "msg": f"Hi {request.get('user')['account_name']}"
-   }
--- lesson04/server/test_module/routes.py
+++
@@ -1,6 +0,0 @@
-from .controllers import send_presence
-
-
-routes = [
-    {'action': 'presence', 'controller': send_presence}
-]
