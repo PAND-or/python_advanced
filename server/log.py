5cc873af08037dd8a04951b3b28974d4e3e4c684
PAND-or <PAND-or@yandex.ru>
2019-03-22 23:37:17 +0300

    hw06 fix

--- server/log.py
+++ server/log.py
@@ -1,12 +1,22 @@
+""""
+2. В декораторе @log реализовать фиксацию функции, из которой была вызвана декорированная. Если имеется такой код:
+@log
+def func_z():
+ pass
+
+def main():
+ func_z()
+...в логе должна быть отражена информация:
+"<дата-время> Функция func_z() вызвана из функции main"
+"""
+
 import inspect
+from server_log_config import logger
 
 def log(func):
     def wrap(request, *args, **kwargs):
-        print('*' * 50)
-
-        print(func)
-        print(request)
-        #print(inspect.function(request))
-        print('*' * 50)
+        file_name = inspect.currentframe().f_back.f_code.co_filename
+        mname = inspect.getmodulename(file_name)
+        logger.info(f'function  {func.__name__}{args} call from {mname}')
         return func(request, *args, **kwargs)
     return wrap
